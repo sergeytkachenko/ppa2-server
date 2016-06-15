73627c0a592898fde71248f13a57c11ab7361a7e
s.tkachenko <serg.tkachenko@hotmail.com>
2016-06-16 01:14:55 +0300

    added data

--- db/import.sh
+++ db/import.sh
@@ -4,11 +4,11 @@ DRIVER="com.mysql.jdbc.Driver"
 CLASSPATH="liquibase-3.4.1-bin/jdbc-driver/mysql-connector-java-5.0.8-bin.jar"
 CONNECTION_STRING="jdbc:mysql://localhost/$DB?useUnicode=true&characterEncoding=UTF-8"
 DB_USER="root"
-DB_PASS="1665017"
+DB_PASS="!Prisoner31!"
 
 if [[ -n "$1" ]]; then
     mysql -u$DB_USER -p$DB_PASS -e "DROP DATABASE IF EXISTS $DB;"
-	mysql -u$DB_USER -p$DB_PASS -e "CREATE DATABASE IF NOT EXISTS $DB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+	mysql -u$DB_USER -p$DB_PASS -e "CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
 fi
 
 ./liquibase-3.4.1-bin/liquibase --changeLogFile="changelogs/structure.xml" \
