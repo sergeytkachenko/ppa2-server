484eb0756f818155de3a77911fd38e9dcdf2a7d3
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2016-06-16 23:27:10 +0300

    fix

--- db/import.sh
+++ db/import.sh
@@ -4,11 +4,12 @@ DRIVER="com.mysql.jdbc.Driver"
 CLASSPATH="liquibase-3.4.1-bin/jdbc-driver/mysql-connector-java-5.0.8-bin.jar"
 CONNECTION_STRING="jdbc:mysql://localhost/$DB?useUnicode=true&characterEncoding=UTF-8"
 DB_USER="root"
-DB_PASS="'!Prisoner31!'"
+DB_PASS="1665017"
 
 if [[ -n "$1" ]]; then
-    mysql -u$DB_USER -p$DB_PASS -e "DROP DATABASE IF EXISTS $DB;"
-    mysql -u$DB_USER -p$DB_PASS -e "CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    mysqladmin drop ppa-server -u$DB_USER -p$DB_PASS -f
+    mysqladmin create ppa-server -u$DB_USER -p$DB_PASS -f
+    # mysql -u$DB_USER -p$DB_PASS -e "CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
 fi
 
 ./liquibase-3.4.1-bin/liquibase --changeLogFile="changelogs/structure.xml" \
